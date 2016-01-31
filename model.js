1a76c4c6a0995b82d6ef37111d171e2ffba8fd0e
Deepak Sharma <dpkshrma01@gmail.com>
2016-01-31 17:34:06 +0530

    Remove WET code
    This commit introduces a data validation function which essentially
    validates objects having constructor 'Object'. This is used at three
    places : model ctor, and twice in check_object function.
    
    'check_object' function takes data of type 'object' as input (which
    can have either Object or Array as ctor) and validates it against its
    corresponding schema input.

--- model.js
+++ model.js
@@ -6,13 +6,18 @@ var prop_err = "Invalid Property : %s\n";
 var dt_err   = "Invalid datatype for property: %s\n Required: %s\n Found: %s\n";
 var obj_err  = "Invalid Object for property: %s\n";
 
-
 function model(schema, data){
+    var model_obj = this;
+    validate_data(schema, data, function(prop){
+        model_obj[prop] = data[prop];
+    });
+}
+
+function validate_data(schema, data, callback){
     if (Object.keys(schema).length !== Object.keys(data).length)
         throw new Error("Invalid object length");
 
-    for(prop in data){
-
+    for(var prop in data){
         if (prop in schema)
             var schema_item_type = typeof schema[prop];
         else{
@@ -21,42 +26,27 @@ function model(schema, data){
         }
 
         if (schema_item_type === 'object'){
-            check_object(data[prop], schema[prop], prop);
+            check_object(data[prop], schema[prop]);
         }
         else if(data[prop].constructor !== schema[prop]){
             var err = sprintf(dt_err, prop, schema[prop].name, data[prop].constructor.name);
             throw new Error(err);
         }
-
-        this[prop] = data[prop];
+        if (typeof callback !== 'undefined'){
+            callback(prop);
+        }
     }
+
 }
 
-function check_object(data_item, schema_item, prop){
+
+function check_object(data_item, schema_item){
     var data_ctor = data_item.constructor,
         sch_ctor = schema_item.constructor;
 
     if (data_ctor === sch_ctor){
         if (sch_ctor === Object){
-            if (Object.keys(schema_item).length !== Object.keys(data_item).length)
-                throw new Error("Invalid object length");
-
-            for(prop in data_item){
-                if (prop in schema_item)
-                    var schema_item_type = typeof schema_item[prop];
-                else{
-                    var err = sprintf(prop_err, prop)
-                    throw new Error(err);
-                }
-
-                if (schema_item_type === 'object'){
-                    check_object(data_item[prop], schema_item[prop], prop);
-                }
-                else if(data_item[prop].constructor !== schema_item[prop]){
-                    var err = sprintf(dt_err, prop, schema_item[prop].name, data_item[prop].constructor.name);
-                    throw new Error(err);
-                }
-            }
+            validate_data(schema_item, data_item);
         }
         else if (sch_ctor === Array){
             schema_item = schema_item[0];
@@ -67,25 +57,7 @@ function check_object(data_item, schema_item, prop){
 
             for (var i = 0; i < data_item.length; i++) {
                 if (arr_child_is_object) {
-                    if(Object.keys(data_item[i]).length !== Object.keys(schema_item).length)
-                        throw new Error("Invalid object length");
-                    for(prop in data_item[i]){
-                        if (prop in schema_item)
-                            var schema_item_type = typeof schema_item[prop];
-                        else{
-                            var err = sprintf(prop_err, prop)
-                            throw new Error(err);
-                        }
-
-                        if (schema_item_type === 'object'){
-                            check_object(data_item[i][prop], schema_item[prop], prop);
-                        }
-                        else if(data_item[i][prop].constructor !== schema_item[prop]){
-                            console.log(schema_item_type);
-                            var err = sprintf(dt_err, prop, schema_item[prop].name, data_item[i][prop].constructor.name);
-                            throw new Error(err);
-                        }
-                    }
+                    validate_data(schema_item, data_item[i]);
                 }
                 else{
                     if(Object.keys(schema_item).length >1)
