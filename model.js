7a8b42f20423371dba4fbbdb317745b57a39022f
Deepak Sharma <dpkshrma01@gmail.com>
2016-02-01 01:20:57 +0530

    Cleanup Error msgs, add comments

--- model.js
+++ model.js
@@ -1,12 +1,20 @@
 
 var sprintf = require('sprintf');
 
-// errors
-var prop_err = "Invalid Property : %s\n";
-var dt_err   = "Invalid datatype for property: %s\n Required: %s\n Found: %s\n";
-var obj_err  = "Invalid Object for property: %s\n";
+// Errors
+var prop_err    = "Invalid Property : %s\n";
+var dt_err      = "Invalid datatype for property: %s\n Required: %s\n Found: %s\n";
+var obj_err     = "Invalid Object for property: %s\n";
+var obj_len_err = "Invalid object length";
 
-function model(schema, data){
+/**
+ * Model Class
+ * @param  {object} schema describes the nosql doc format
+ * @param  {object} data   nosql doc
+ * @return {none}          none
+ */
+function Model(schema, data){
+    // read only schema property
     Object.defineProperty(this, "schema", {
         enumerable: false,
         value: schema
@@ -17,9 +25,16 @@ function model(schema, data){
     });
 }
 
+/**
+ * Validates Data Object against given Schema
+ * @param  {object}   schema   describes the nosql "doc/doc item" format
+ * @param  {object}   data     nosql "doc/doc item"
+ * @param  {Function} callback If no exception occurs, add to Model object
+ * @return {none}              none
+ */
 function validate_object(schema, data, callback){
     if (Object.keys(schema).length !== Object.keys(data).length)
-        throw new Error("Invalid object length");
+        throw new Error(obg_len_err);
 
     for(var prop in data){
         if (prop in schema)
@@ -43,6 +58,14 @@ function validate_object(schema, data, callback){
 
 }
 
+/**
+ * Checks whether the data_item's ctor is Object or Array,
+ * validates with ctor of schema_item, and calls validate_object
+ * on each child objects.
+ * @param  {object} data_item   data property value
+ * @param  {object} schema_item schema property value
+ * @return {none}               none
+ */
 function check_object(data_item, schema_item){
     var data_ctor = data_item.constructor,
         sch_ctor = schema_item.constructor;
@@ -52,19 +75,24 @@ function check_object(data_item, schema_item){
             validate_object(schema_item, data_item);
         }
         else if (sch_ctor === Array){
+            // Schema item must always contain one object
+            // describing its data values
             schema_item = schema_item[0];
 
+            // Array containing single values, will have single
+            // key(type) object containing value ctor
             var arr_child_is_object = true;
             if ("type" in schema_item)
                 arr_child_is_object = false;
 
+            // Iterate over data item values
             for (var i = 0; i < data_item.length; i++) {
                 if (arr_child_is_object) {
                     validate_object(schema_item, data_item[i]);
                 }
                 else{
                     if(Object.keys(schema_item).length >1)
-                        throw new Error("Invalid object length");
+                        throw new Error(obg_len_err);
                     if(schema_item['type'] !== data_item[i].constructor){
                         var err = sprintf(dt_err, prop, schema_item[prop].name, data_item[prop].constructor.name);
                         throw new Error(err);
@@ -74,22 +102,24 @@ function check_object(data_item, schema_item){
         }
     }
     else{
+        // schema item ctor != data item ctor
         var err = sprintf(obj_err, prop);
         throw new Error(err);
     }
 }
 
-model.prototype.get = function(prop){
+Model.prototype.get = function(prop){
     if (typeof prop === 'undefined')
         return this;
     if (typeof this.schema[prop] === 'undefined'){
-        throw new Error("Invalid Property");
+        var err = sprintf(prop_err, prop);
+        throw new Error(err);
     }
     return this[prop];
 }
 
-model.prototype.get_schema = function(){
+Model.prototype.get_schema = function(){
     return this.schema;
 }
 
-module.exports = model;
+module.exports = Model;
